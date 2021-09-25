82da4176ad7eb2b6744217139851de84ea79bba9
HeavyRain266 <ignacy423@gmail.com>
2021-09-25 21:56:10 +0200

    format CSS and JS

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 function showTime() {
 	const date = new Date();
 
-	let today = date.toLocaleString('en', {weekday: 'long'});
-	let hour = date.toLocaleString('pl', {hour: '2-digit'}); // use 24h time format instead of 12
-	let minute = date.toLocaleString('en', {minute: '2-digit'});
-	let second = date.toLocaleString('en', {second: '2-digit'});
-	let day = date.toLocaleString('en', {day: '2-digit'});
-	let month = date.toLocaleString('en', {month: '2-digit'});
-	let year = date.toLocaleString('en', {year: 'numeric'});
+	let today = date.toLocaleString("en", { weekday: "long" });
+	let hour = date.toLocaleString("pl", { hour: "2-digit" }); // use 24h time format
+	let minute = date.toLocaleString("en", { minute: "2-digit" });
+	let second = date.toLocaleString("en", { second: "2-digit" });
+	let day = date.toLocaleString("en", { day: "2-digit" });
+	let month = date.toLocaleString("en", { month: "2-digit" });
+	let year = date.toLocaleString("en", { year: "numeric" });
 
-	minute = addZero(minute)
-	second = addZero(second)
+	minute = addZero(minute);
+	second = addZero(second);
 
-	document.getElementById("date").innerHTML = today + ', ' + hour + ':' + minute + ':' + second + ' | ' + day + '/' + month + '/' + year;
-	setTimeout(showTime, 0)
+	document.getElementById(
+		"date"
+	).innerHTML = `${today}, ${hour}:${minute}:${second} | ${day}/${month}/${year}`;
+	setTimeout(showTime, 0);
 }
 
 function addZero(i) {
-	if (i < 10) (i = '0' + i);
+	if (i < 10) i = "0" + i;
 	return i;
 }
 
