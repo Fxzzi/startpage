56f2415d16cf65c5268f23263e9b5ea1bbe8afc0
faaris <faaris@Kappa>
2023-03-12 15:32:10 +0000

    rework color setting, add preload on images, merge style.css into one

--- src/colorHelper.js
+++ src/colorHelper.js
@@ -2,60 +2,48 @@
 // This uses function applyColors to apply colours to a different ids and classes on the site
 // localStorage is used to retain these changes across reloads
 
-// Define applyColors function
-function applyColors(counter, colors) {
-  
-  // Set up constants for some of the items we want to edit
-  const body = document.body;
-  const date = document.querySelector("#date");
-  const anchors = document.querySelectorAll('a');
-  const aElements = document.querySelectorAll("url-list a");
-  const listItems = document.querySelectorAll("url-list li:first-child");
-
-  // Change body colors
-  body.style.color = colors[counter].color;
-  body.style.background = colors[counter].background;
-  date.style.color = colors[counter].hlcol;
-
-  // Change link colors for all 'a' elements
-  anchors.forEach(anchor => {
-    anchor.style.color = colors[counter].linkcol;
-  });
-
-  // Change link colors and add hover events for 'a' elements within 'url-list' elements
-  aElements.forEach(a => {
-    a.style.color = colors[counter].linkcol;
-    // Checks when link is hovered
-    a.addEventListener("mouseenter", function() {
-      // Sets to hover color when hovered
-      this.style.color = colors[counter].hovercol;
-    });    
-    // Checks when mouse is not hovering
-    a.addEventListener("mouseleave", function() {
-      // Sets back to original color.
-      this.style.color = colors[counter].linkcol;
-    });
-  });
-
-  // Change first child color of each 'url-list' element (aka every heading)
-  listItems.forEach(item => {
-    item.style.color = colors[counter].hlcol;
-  });
-}
-
-// Sets up constants for our image changing portion
+// Sets up variables for our image changing portion
+// 1st one grabs the image div, second grabs the image itself
 const imageDiv = document.getElementById("imageDiv");
 const image = document.getElementById("image");
+
 // Sets paths for image carousel
 const imageFiles = ["../src/images/image1.jpg", "../src/images/image2.jpg", "../src/images/image3.jpg"];
 
+// Set color variables for hex codes we use more than once
+const tokyoNightPurple = "#bb9af7";
+const tokyoNightBackground = "#1a1b26"
+const tokyoNightBlue = "#7aa2f7"
+
 // Define color arrays for each image
 const colors = [
-  // Create array of color variables for each 'theme'
-  { color: "#bb9af7", background: "#1a1b26", hlcol: "#7aa2f7", linkcol: "#CDD6F4", hovercol: "#e0af68" }, // colors for image 1
+  { color: tokyoNightPurple, background: tokyoNightBackground, hlcol: tokyoNightBlue, linkcol: "#CDD6F4", hovercol: "#e0af68" }, // colors for image 1
   { color: "#D2C7CB", background: "#15191d", hlcol: "#28725A", linkcol: "#9fadc6", hovercol: "#e0af68" }, // colors for image 2
-  { color: "#bb9af7", background: "#1a1b26", hlcol: "#7aa2f7", linkcol: "#717189", hovercol: "#CDD6F4" }, // colors for image 3
+  { color: tokyoNightPurple, background: tokyoNightBackground, hlcol: tokyoNightBlue, linkcol: "#717189", hovercol: "#CDD6F4" }, // colors for image 3
 ];
+
+// Define a function to preload the images
+function preloadImages() {
+  // Loop through the array of image files
+  for (let i = 0; i < imageFiles.length; i++) {
+    // Create a new Image object for each image file
+    const img = new Image();
+    // Set the source attribute of the Image object to the image file path
+    img.src = imageFiles[i];
+  }
+}
+
+// Define applyColors function to change color properties depending on the counter
+function applyColors(counter, colors) {
+  document.documentElement.style.setProperty('--primary-color', colors[counter].linkcol);
+  document.documentElement.style.setProperty('--secondary-color', colors[counter].color);
+  document.documentElement.style.setProperty('--accent-color', colors[counter].hlcol);
+  document.documentElement.style.setProperty('--link-accent-color', colors[counter].hovercol);
+  document.documentElement.style.setProperty('--bg-color', colors[counter].background);
+}
+
+// Call the preloadImages function to start loading the images
+preloadImages();
 
 // Changes the image to the one linked to our counter
 image.src = imageFiles[counter];
@@ -83,4 +71,3 @@ imageDiv.addEventListener("click", function() {
      // 200 sets time of 0.2s for transition
   }, 200);
 });
-
