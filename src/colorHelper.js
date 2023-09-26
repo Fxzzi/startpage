19f6ea66748da6ea72045c3534a1873a9a353ffe
Fazzi <faaris.ansari@proton.me>
2023-09-26 17:50:14 +0100

    lint, change back to JBM because I can never make my mind up

--- src/colorHelper.js
+++ src/colorHelper.js
@@ -6,43 +6,80 @@
 // 1st one grabs the image div, second grabs the image itself
 const imageDiv = document.getElementById("imageDiv");
 const image = document.getElementById("image");
-const mainBody = document.getElementById("mainBody")
+const mainBody = document.getElementById("mainBody");
 
 // Sets paths for image carousel
-const imageFiles = ["../src/images/image1.webp", "../src/images/image2.webp", "../src/images/image3.webp"];
+const imageFiles = [
+	"../src/images/image1.webp",
+	"../src/images/image2.webp",
+	"../src/images/image3.webp",
+];
 
 // Set color variables for hex codes we use more than once
 const tokyoNightPurple = "#bb9af7";
-const tokyoNightBackground = "#1a1b26"
-const tokyoNightBlue = "#7aa2f7"
-const tokyoNightForeground = "#CDD6F4"
-const tokyoNightYellow = "#E0AF68"
+const tokyoNightBackground = "#1a1b26";
+const tokyoNightBlue = "#7aa2f7";
+const tokyoNightForeground = "#CDD6F4";
+const tokyoNightYellow = "#E0AF68";
 
 // Define color arrays for each image
 const colors = [
-  { color: tokyoNightYellow, background: tokyoNightBackground, hlcol: tokyoNightBlue, linkcol: "#A8B8CF", hovercol: tokyoNightPurple }, // colors for image 1
-  { color: "#D2C7CB", background: "#15191d", hlcol: "#28725A", linkcol: "#9fadc6", hovercol: "#9B5856" }, // colors for image 2
-  { color: tokyoNightPurple, background: tokyoNightBackground, hlcol: tokyoNightBlue, linkcol: tokyoNightForeground, hovercol: tokyoNightYellow },  // colors for image 3
+	{
+		color: tokyoNightYellow,
+		background: tokyoNightBackground,
+		hlcol: tokyoNightBlue,
+		linkcol: "#A8B8CF",
+		hovercol: tokyoNightPurple,
+	}, // colors for image 1
+	{
+		color: "#D2C7CB",
+		background: "#15191d",
+		hlcol: "#28725A",
+		linkcol: "#9fadc6",
+		hovercol: "#9B5856",
+	}, // colors for image 2
+	{
+		color: tokyoNightPurple,
+		background: tokyoNightBackground,
+		hlcol: tokyoNightBlue,
+		linkcol: tokyoNightForeground,
+		hovercol: tokyoNightYellow,
+	}, // colors for image 3
 ];
 
 // Define a function to preload the images
 function preloadImages() {
-  // Loop through the array of image files
-  for (let i = 0; i < imageFiles.length; i++) {
-    // Create a new Image object for each image file
-    const img = new Image();
-    // Set the source attribute of the Image object to the image file path
-    img.src = imageFiles[i];
-  }
+	// Loop through the array of image files
+	for (let i = 0; i < imageFiles.length; i++) {
+		// Create a new Image object for each image file
+		const img = new Image();
+		// Set the source attribute of the Image object to the image file path
+		img.src = imageFiles[i];
+	}
 }
 
 // Define applyColors function to change color properties depending on the counter
 function applyColors(counter, colors) {
-  document.documentElement.style.setProperty('--primary-color', colors[counter].linkcol);
-  document.documentElement.style.setProperty('--secondary-color', colors[counter].color);
-  document.documentElement.style.setProperty('--accent-color', colors[counter].hlcol);
-  document.documentElement.style.setProperty('--link-accent-color', colors[counter].hovercol);
-  document.documentElement.style.setProperty('--bg-color', colors[counter].background);
+	document.documentElement.style.setProperty(
+		"--primary-color",
+		colors[counter].linkcol
+	);
+	document.documentElement.style.setProperty(
+		"--secondary-color",
+		colors[counter].color
+	);
+	document.documentElement.style.setProperty(
+		"--accent-color",
+		colors[counter].hlcol
+	);
+	document.documentElement.style.setProperty(
+		"--link-accent-color",
+		colors[counter].hovercol
+	);
+	document.documentElement.style.setProperty(
+		"--bg-color",
+		colors[counter].background
+	);
 }
 
 // Call the preloadImages function to start loading the images
@@ -56,21 +93,21 @@ applyColors(counter, colors);
 
 // Listens for clicks on the image
 imageDiv.addEventListener("click", function() {
-  // Add a class to the image element
-  image.classList.add("fade-out");
+	// Add a class to the image element
+	image.classList.add("fade-out");
 
-  // Wait for the transition to complete
-  setTimeout(() => {
-    // Remove the class from the image element
-    image.classList.remove("fade-out");
+	// Wait for the transition to complete
+	setTimeout(() => {
+		// Remove the class from the image element
+		image.classList.remove("fade-out");
 
-    // Change the image source
-    counter = (counter + 1) % imageFiles.length;
-    localStorage.setItem("counter", counter);
-    image.src = imageFiles[counter];
+		// Change the image source
+		counter = (counter + 1) % imageFiles.length;
+		localStorage.setItem("counter", counter);
+		image.src = imageFiles[counter];
 
-    // Apply colors
-    applyColors(counter, colors);
-     // 200 sets time of 0.2s for transition
-  }, 200);
+		// Apply colors
+		applyColors(counter, colors);
+		// 200 sets time of 0.2s for transition
+	}, 200);
 });
--- src/dateHelper.js
+++ src/dateHelper.js
@@ -7,37 +7,33 @@ const dateElement = document.getElementById("date");
 
 // Defines funtion to format the time correctly
 function showTime() {
+	// Uses Date to retrieve information
+	const date = new Date();
 
-  // Uses Date to retrieve information
-  const date = new Date();
+	// Sets options for correct formatting
+	const options = {
+		weekday: "long",
+		hour: "2-digit",
+		minute: "2-digit",
+		second: "2-digit",
+		day: "2-digit",
+		month: "2-digit",
+		year: "2-digit",
+		hour12: false,
+	};
 
-  // Sets options for correct formatting
-  const options = {
-    weekday: "long",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-    day: "2-digit",
-    month: "2-digit",
-    year: "2-digit",
-    hour12: false,
-  };
+	// Formats the date in DD/MM/YY instead of MM/DD/YY
+	const formattedDate = date.toLocaleString("en-GB", options);
 
-  // Formats the date in DD/MM/YY instead of MM/DD/YY
-  const formattedDate = date.toLocaleString("en-GB", options);
+	// Extract day of week, date, and time from formattedDate
+	const [dayOfWeek, dateStr, time] = formattedDate.split(", ");
 
-  // Extract day of week, date, and time from formattedDate
-  const [dayOfWeek, dateStr, time] = formattedDate.split(", ");
-
-  // Format so it shows Day, HH:MM:SS | DD/MM/YY
-  dateElement.innerHTML = `${dayOfWeek}, ${time} | ${dateStr}`;
+	// Format so it shows Day, HH:MM:SS | DD/MM/YY
+	dateElement.innerHTML = `${dayOfWeek}, ${time} | ${dateStr}`;
 }
 
 // Sets the function to repeat every 1000ms (1s)
 setInterval(showTime, 1000);
 
 // Calls the function
-showTime()
-
-
-
+showTime();
